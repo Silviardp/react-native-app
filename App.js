8ffdbd1823000aebbeec8071a6df2d9fec224361
Silvia Rocha <silviadprocha@gmail.com>
2020-05-22 18:19:21 +0900

    added handle press function + numberOfLines limited of 1 for text

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello react Native</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello react Native
+      </Text>
     </SafeAreaView>
   );
 }
