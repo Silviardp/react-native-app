fa7b0de27ce167a2655ffcf7ed57ed633f0e46fe
Silvia Rocha <silviadprocha@gmail.com>
2020-05-22 18:50:22 +0900

    add touchable opacity

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello react Native</Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableOpacity onPress={() => console.log("image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
