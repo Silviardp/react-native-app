03ee3c8bb2291d12c8cc6187a6c387460b128d29
Silvia Rocha <silviadprocha@gmail.com>
2020-05-22 18:37:27 +0900

    add image

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text pressed");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello react Native
-      </Text>
+      <Text>Hello react Native</Text>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
     </SafeAreaView>
   );
 }
@@ -16,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
-    // alignItems: "center",
-    // justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
